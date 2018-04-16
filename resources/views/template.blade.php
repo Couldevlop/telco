62c25443d2af1b2b705b9d17e61e28f9908de92d
THOMAS COULIBALY <coulwao@gmail.com>
2018-04-16 14:24:42 +0100

    second commit

---
+++ resources/views/template.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Ubusina - Responsive HTML5 Template</title>
+	<link rel="stylesheet" href="{{URL::asset('css/font-awesome.min.css')}}">
+	<link rel="stylesheet" href="{{URL::asset('css/bootstrap.min.css')}}">
+	<link rel="stylesheet" href="{{URL::asset('css/style.css')}}">
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:600italic,400,800,700,300' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=BenchNine:300,400,700' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+@include('partials._menu')
+@yield('content')
+@include('partials/_slider')
+@include('partials/_about')
+@include('partials/_services')
+@include('partials/_team')
+@include('partials/_map')
+@include('partials/_contact')
+
+
+
+<!-- script tags
+	============================================================= -->
+	<script src="{{URL::asset('js/jquery-2.1.1.js')}}"></script>
+	<script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+	<script src="{{URL::asset('js/gmaps.js')}}"></script>
+	<script src="{{URL::asset('js/smoothscroll.js')}}"></script>
+	<script src="{{URL::asset('js/bootstrap.min.js')}}"></script>
+	<script src="{{URL::asset('js/custom.js')}}"></script>
+</body>
+<!-- footer starts here -->
+			<footer class="footer clearfix">
+				<div class="container">
+					<div class="row">
+						<div class="col-xs-6 footer-para">
+							<p>&copy;freshDesignweb.com All right reserved</p>
+						</div>
+
+						<div class="col-xs-6 text-right">
+							<a href=""><i class="fa fa-facebook"></i></a>
+							<a href=""><i class="fa fa-twitter"></i></a>
+							<a href=""><i class="fa fa-skype"></i></a>
+						</div>
+					</div>
+				</div>
+			</footer>
+
+</html>
+
