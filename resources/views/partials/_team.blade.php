e9be6ac535f15f45fdea26511769a46918a76930
koné Salimata <misskone690@gmail.com>
2018-04-19 15:59:07 +0200

    first commit

--- resources/views/partials/_team.blade.php
+++ resources/views/partials/_team.blade.php
@@ -4,9 +4,9 @@
 				<div class="container">
 					<div class="row">
 						<div class="team-heading text-center">
-							<h2>Notre equipe</h2>
+							<h2>Notre Equipe</h2>
 
-							<h4>Un équipe motivée, dynamique et hautement experimenter à votre service pour offrir la qualité du service via des solutions adequates</h4>
+							<h4>Nous disposons de plusieurs techniciens et de cinq ingénieurs ayant les capacités techniques et professionnelles. Le nombre peut croitre avec des agnts temporaires de qualité en fonction des offres a exécuter</h4>
 						</div>
 
 						<div class="col-md-2 single-member col-sm-4">
@@ -16,8 +16,9 @@
 
 							<div class="person-detail">
 								<div class="arrow-bottom"></div>
-								<h3>Mr. Graham</h3>
-								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
+								<h3>Mr. YOUIN</h3>
+								<p>Ivoirien(ingénieur électronicien), 20ans d'experience dont 18 ans dans la téléphonie mobile via IVOIRIS, ORANGE et GLO MOBILE Nigeria
+								</p>
 							</div>
 						
 						</div>
@@ -26,8 +27,8 @@
 
 							<div class="person-detail">
 								<div class="arrow-top"></div>
-								<h3>Mr. David</h3>
-								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
+								<h3>Mr. KOUENON</h3>
+								<p>Ivoirien(ingénieur ESCA) 28 ans d'experience dans la gestion d'entreprise </p>
 							</div>
 							<div class="person">
 								<img class="img-responsive" src="img/item2.jpg" alt="member-2">
@@ -39,16 +40,16 @@
 							</div>
 							<div class="person-detail">
 								<div class="arrow-bottom"></div>
-								<h3>Mr. Hovid</h3>
-								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
+								<h3>Mr. VIDELOUP</h3>
+								<p>Français (ingénieur economiste), 20ans d'experience, expert dans la recherche et le developpement de nouveaux marchés.</p>
 							</div>
 						</div>
 
 						<div class="col-md-2 single-member col-sm-4">
 							<div class="person-detail">
 								<div class="arrow-top"></div>
-								<h3>Mr Jasak</h3>
-								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
+								<h3>Mr COULIBALY</h3>
+								<p>Responsable service informatique et developpement</p>
 							</div>
 							<div class="person">
 								<img class="img-responsive" src="img/item4.jpg" alt="member-4">
@@ -62,8 +63,8 @@
 
 							<div class="person-detail">
 								<div class="arrow-bottom"></div>
-								<h3>Mr. Joy Ka</h3>
-								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
+								<h3>Mr. GMONROU </h3>
+								<p>Responsable marketing et publicité et de l'entreprise</p>
 							</div>
 						</div>
 
@@ -71,8 +72,8 @@
 
 							<div class="person-detail">
 								<div class="arrow-top"></div>
-								<h3>Mr. Mikari</h3>
-								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
+								<h3>Mlle. KONE</h3>
+								<p>Développeur web et mobile</p>
 							</div>
 							<div class="person">
 								<img class="img-responsive" src="img/item6.jpg" alt="member-5">
